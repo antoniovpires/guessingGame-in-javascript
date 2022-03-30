b992a1bea459e58b2959f34f6ff73bc4cc8c5d88
Antônio Pires <antoniovpiresnt@gmail.com>
2022-03-30 17:37:59 -0300

    update-0.4
    Somo improvements on the UX and I've implemented the Higherscore!

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function downScore() {
 let answer = Math.trunc(Math.random(0, 20) * (20 - 1) + 1)
 console.log(answer)
 let score = Number(document.querySelector('.score').textContent);
+let higherScore = Number(document.querySelector('.highscore').textContent);
 
 
 document.querySelector('.again').addEventListener('click', function(){
@@ -19,6 +20,7 @@ document.querySelector('.again').addEventListener('click', function(){
     document.querySelector('.guess').value = NaN;
     document.querySelector('body').style.backgroundColor = '#222'
     document.querySelector('.number').textContent = '?';
+    score = Number(document.querySelector('.score').textContent);
 })
 
 
@@ -31,11 +33,20 @@ document.querySelector('.check').addEventListener('click', function(){
         document.querySelector('.message').textContent = "That's it! You've got it right!!";
         document.querySelector('.number').textContent = answer;
         document.querySelector('body').style.backgroundColor = '#60b347';
+        if (score > higherScore) {
+            document.querySelector('.highscore').textContent = score
+        }       
     } else if (guess > answer) {
         document.querySelector('.message').textContent = 'Too high! Try another number!!'
         downScore()
     } else if (guess < answer) {
         document.querySelector('.message').textContent = 'Too low! Try another number!!'
         downScore()
+    }
+    if (score <= 0) {
+        document.querySelector('.number').textContent = 'G A M E  O V E R';
+        document.querySelector('.message').textContent = 'Restart the game!' 
+        document.querySelector('.number').style.width = '50rem';
+        document.querySelector('body').style.backgroundColor = '#b10707';
     }
 })
