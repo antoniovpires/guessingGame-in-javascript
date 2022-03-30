b4cd87c5a4bd7a973da1f696c3ad9ac92e453f04
Antônio Pires <antoniovpiresnt@gmail.com>
2022-03-30 16:12:29 -0300

    update_0.3

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+function downScore() {
+    score--
+    document.querySelector('.score').textContent = score
+}
+
+
+let answer = Math.trunc(Math.random(0, 20) * (20 - 1) + 1)
+console.log(answer)
+let score = Number(document.querySelector('.score').textContent);
+
+
+document.querySelector('.again').addEventListener('click', function(){
+    document.querySelector('.score').textContent = 20;
+    answer = Math.trunc(Math.random(0, 20) * (20 - 1) + 1)
+    console.log(answer)
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.guess').value = NaN;
+    document.querySelector('body').style.backgroundColor = '#222'
+})
+
+
+document.querySelector('.check').addEventListener('click', function(){
+    let guess = Number(document.querySelector('.guess').value);
+    if (!guess) {
+        document.querySelector('.message').textContent = 'Please, insert a number bigger than 0!';
+        downScore()
+    } else if (guess === answer) {
+        document.querySelector('.message').textContent = "That's it! You've got it right!!";
+        document.querySelector('.number').textContent = answer;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+    } else if (guess > answer) {
+        document.querySelector('.message').textContent = 'Too high! Try another number!!'
+        downScore()
+    } else if (guess < answer) {
+        document.querySelector('.message').textContent = 'Too low! Try another number!!'
+        downScore()
+    }
+})
+>>>>>>> Stashed changes
